20bdcb225406c955c7ef9478d3a41377453ee686
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-07-13 16:28:38 -0400

    Extending Routes and Product Model

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,18 @@ const path = require("path");
 
 const express = require("express");
 
-const productController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", productController.getProducts);
+router.get("/products", shopController.getProducts);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/checkout", shopController.getCheckout);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/", shopController.getIndex);
 
 module.exports = router;
