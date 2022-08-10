81b456b370cd33955f057b46c50c0a0f161b0be2
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-08-10 15:48:52 -0400

    Adding Payment with Stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,9 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
@@ -130,7 +133,54 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              currency: "cad",
+              product_data: {
+                name: p.productId.title,
+              },
+              unit_amount: p.productId.price * 100,
+            },
+            quantity: p.quantity,
+          };
+        }),
+        mode: "payment",
+        success_url: `${req.protocol}://${req.get("host")}/checkout/success`,
+        cancel_url: `${req.protocol}://${req.get("host")}/checkout/cancel`,
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
     .then((user) => {
